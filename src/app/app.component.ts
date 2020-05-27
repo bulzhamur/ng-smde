2c7012831901fac6458f0d66bcfabb9046781ac4
bulat <bulzhamur@gmail.com>
2020-05-27 22:48:07 +0800

    simplemde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {SimplemdeComponent} from 'ngx-simplemde';
+import {SimplemdeOptions} from 'ngx-simplemde/src/config';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'simplemde-test';
+export class AppComponent implements OnInit {
+  @ViewChild('simplemde', { static: true }) private readonly simplemde: SimplemdeComponent;
+
+  options: SimplemdeOptions = {
+    toolbar: true
+  };
+
+  demo = `# This one is bare
+You can also choose to hide the statusbar and/or toolbar for a simple and clean appearance. This one also checks for misspelled words as you type!`;
+
+  ngOnInit(): void {
+    this.simplemde.setOptions('lineNumbers', true);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { SimplemdeModule } from 'ngx-simplemde';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, SimplemdeModule.forRoot({}) ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
